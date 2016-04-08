7a87c2a78ebb53d71ac3abb65836261aedee599e
Anastassia Loukina <aloukina@ets.org>
2016-04-08 17:09:30 -0400

    Added test

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1462,6 +1462,35 @@ def test_run_experiment_lr_candidate_same_as_id():
     yield check_report, html_report
 
 
+def test_run_experiment_lr_candidate_same_as_id_candidate():
+    # basic LR experiment but with `candidate_column` set to the
+    # same value as `id_column` and both values are set to 'candidate'
+
+    source = 'lr-candidate-same-as-id-candidate'
+    experiment_id = 'lr_candidate_same_as_id_candidate'
+    config_file = join(test_dir,
+                       'data',
+                       'experiments',
+                       source,
+                       '{}.json'.format(experiment_id))
+    do_run_experiment(source, experiment_id, config_file)
+    output_dir = join('test_outputs', source, 'output')
+    expected_output_dir = join(test_dir, 'data', 'experiments', source, 'output')
+    html_report = join('test_outputs', source, 'report', '{}_report.html'.format(experiment_id))
+
+    csv_files = glob(join(output_dir, '*.csv'))
+    for csv_file in csv_files:
+        csv_filename = basename(csv_file)
+        expected_csv_file = join(expected_output_dir, csv_filename)
+
+        if exists(expected_csv_file):
+            yield check_csv_output, csv_file, expected_csv_file
+
+    yield check_all_csv_exist, csv_files, experiment_id, 'rsmtool'
+    yield check_scaled_coefficients, source, experiment_id
+    yield check_report, html_report
+
+
 def test_run_experiment_lr_compare():
 
     # basic rsmcompare experiment comparing a LinearRegression
