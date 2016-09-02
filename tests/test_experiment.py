294cba42e09ad51d2bf4c29506e199e802ee1291
Nitin Madnani <nmadnani@ets.org>
2016-09-02 17:26:11 -0400

    Change `.report` to `_report` for `rsmcompare`.

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1661,7 +1661,7 @@ def test_run_experiment_lr_compare():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups.report.html')
+    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups_report.html')
     yield check_report, html_report
 
 
@@ -1678,7 +1678,7 @@ def test_run_experiment_lr_compare_different_experiments():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_baseline_vs_lr_with_FEATURE8_and_zero_scores.report.html')
+    html_report = join('test_outputs', source, 'lr_baseline_vs_lr_with_FEATURE8_and_zero_scores_report.html')
     yield check_report, html_report
 
 def test_run_experiment_lr_compare_with_h2():
@@ -1694,7 +1694,7 @@ def test_run_experiment_lr_compare_with_h2():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_with_h2_vs_lr_with_h2.report.html')
+    html_report = join('test_outputs', source, 'lr_with_h2_vs_lr_with_h2_report.html')
     yield check_report, html_report
 
 
@@ -1710,7 +1710,7 @@ def test_run_experiment_lr_compare_with_custom_order():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups.report.html')
+    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups_report.html')
     yield check_report, html_report
 
 
@@ -1726,7 +1726,7 @@ def test_run_experiment_lr_compare_with_chosen_sections():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups.report.html')
+    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups_report.html')
     yield check_report, html_report
 
 
@@ -1743,7 +1743,7 @@ def test_run_experiment_lr_compare_with_custom_sections_and_custom_order():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups.report.html')
+    html_report = join('test_outputs', source, 'lr_subgroups_vs_lr_subgroups_report.html')
     yield check_report, html_report
 
 
@@ -1759,7 +1759,7 @@ def test_run_experiment_linearsvr_compare():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'LinearSVR_vs_LinearSVR.report.html')
+    html_report = join('test_outputs', source, 'LinearSVR_vs_LinearSVR_report.html')
     yield check_report, html_report
 
 
@@ -1775,7 +1775,7 @@ def test_run_experiment_lr_eval_compare():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_eval_with_h2_vs_lr_eval_with_h2.report.html')
+    html_report = join('test_outputs', source, 'lr_eval_with_h2_vs_lr_eval_with_h2_report.html')
     yield check_report, html_report
 
 
@@ -1791,7 +1791,7 @@ def test_run_experiment_lr_eval_tool_compare():
                        'rsmcompare.json')
     do_run_comparison(source, config_file)
 
-    html_report = join('test_outputs', source, 'lr_with_h2_vs_lr_eval_with_h2.report.html')
+    html_report = join('test_outputs', source, 'lr_with_h2_vs_lr_eval_with_h2_report.html')
     yield check_report, html_report
 
 
