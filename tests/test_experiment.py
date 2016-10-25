12354b7615019f1bfc555aa4773c742ad2487df0
Anastassia Loukina <aloukina@ets.org>
2016-10-25 15:07:05 -0400

    Functional tests and json2csv utility

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -82,6 +82,39 @@ def test_run_experiment_lr_old_config():
         yield check_report, html_report
 
 
+def test_run_experiment_lr_feature_json():
+    # basic experiment with a LinearRegression model but using 
+    # feature json file
+
+    source = 'lr-feature-json'
+    experiment_id = 'empWt'
+    config_file = join(test_dir,
+                       'data',
+                       'experiments',
+                       source,
+                       '{}.json'.format(experiment_id))
+
+    # run this experiment but suppress the expected deprecation warnings
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+        do_run_experiment(source, experiment_id, config_file)
+        output_dir = join('test_outputs', source, 'output')
+        expected_output_dir = join(test_dir, 'data', 'experiments', source, 'output')
+        html_report = join('test_outputs', source, 'report', '{}_report.html'.format(experiment_id))
+
+        csv_files = glob(join(output_dir, '*.csv'))
+        for csv_file in csv_files:
+            csv_filename = basename(csv_file)
+            expected_csv_file = join(expected_output_dir, csv_filename)
+
+            if exists(expected_csv_file):
+                yield check_csv_output, csv_file, expected_csv_file
+
+        yield check_all_csv_exist, csv_files, experiment_id, 'rsmtool'
+        yield check_scaled_coefficients, source, experiment_id
+        yield check_report, html_report
+
+
 def test_run_experiment_lr_subset_features():
     # basic experiment with LinearRegression model but using only
     # a subset of all the features in the train/test files
