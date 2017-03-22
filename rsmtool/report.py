d8109fa2e33c0e282484c6deff0ca507d28844e5
Anastassia Loukina <aloukina@ets.org>
2017-03-22 15:03:57 -0400

    Changed the report.py code and the notebook

--- rsmtool/report.py
+++ rsmtool/report.py
@@ -39,6 +39,7 @@ else:
                                     'feature_descriptives',
                                     'features_by_group',
                                     'preprocessed_features',
+                                    'dff',
                                     'consistency',
                                     'model',
                                     'evaluation',
