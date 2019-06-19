64f9304bf2984dcc9b61ea32a6d4ff1aced485bf
Anastassia Loukina <aloukina@ets.org>
2019-06-19 17:25:22 -0400

    created tests

--- tests/test_experiment_rsmeval.py
+++ tests/test_experiment_rsmeval.py
@@ -38,7 +38,8 @@ else:
     param('lr-eval-with-h2-named-sc1', 'lr_eval_with_h2_named_sc1', consistency=True),
     param('lr-eval-with-scaling-and-h2-keep-zeros', 'lr_eval_with_scaling_and_h2_keep_zeros', consistency=True),
     param('lr-eval-with-continuous-human-scores', 'lr_eval_with_continuous_human_scores', consistency=True),
-    param('lr-eval-with-subset-double-scored', 'lr_eval_with_subset_double_scored', consistency=True)
+    param('lr-eval-with-subset-double-scored', 'lr_eval_with_subset_double_scored', consistency=True),
+    param('lr-eval-with-trim-tolerance', 'lr_evaluation_with_trim_tolerance')
 ])
 def test_run_experiment_parameterized(*args, **kwargs):
     if TEST_DIR:
