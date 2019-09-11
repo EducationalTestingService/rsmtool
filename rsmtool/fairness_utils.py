4c794ca8cd5ebdb870552f0024978aca101f0465
Anastassia Loukina <aloukina@users.noreply.github.com>
2019-09-11 16:42:14 -0400

    Update rsmtool/fairness_utils.py
    Co-Authored-By: Nitin Madnani <nmadnani@ets.org>

--- rsmtool/fairness_utils.py
+++ rsmtool/fairness_utils.py
@@ -81,7 +81,7 @@ def get_coefficients(fit, base_category):
     """
     Extract estimates, significance, and confidence intervals
     for members of the group of interest. The names of the
-    predictors are processed to remove the prefix added by Statmodels.
+    predictors are processed to remove the prefix added by `statmodels`.
     The name of the base category is added in parenthesis to the Intercept.
 
     Parameters:
