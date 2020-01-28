Added correct handling of cases with n=1 for correlation computation

rsmtool/analyzer.py
@@ -552,26 +552,33 @@ class Analyzer:
 
         for group, df_group in grouped:
             df_group = df_group.drop(grouping_variable, 1)
-            # if we are asked to include length, that means 'length' is
-            # in the data frame which means that we want to exclude that
-            # before computing the regular marginal and partial correlations
-            if not include_length:
-                df_target_cors[group] = df_group.apply(lambda s:
-                                                       pearsonr(s, df_group[target_variable])[0])
-                df_target_pcorr[group] = partial_correlations(df_group)[target_variable]
+            
+            # first check if we have at least 2 cases and return np.nan otherwise
+            if len(df_group) == 1:
+                df_target_cors[group] = pd.Series(index=df_group.columns)
+                df_target_pcorr[group] = pd.Series(index=df_group.columns)
+                df_target_pcorr_no_length[group] = pd.Series(index=df_group.columns)
             else:
-                df_group_no_length = df_group.drop('length', axis=1)
+                # if we are asked to include length, that means 'length' is
+                # in the data frame which means that we want to exclude that
+                # before computing the regular marginal and partial correlations
+                if not include_length:
+                    df_target_cors[group] = df_group.apply(lambda s:
+                                                           pearsonr(s, df_group[target_variable])[0])
+                    df_target_pcorr[group] = partial_correlations(df_group)[target_variable]
+                else:
+                    df_group_no_length = df_group.drop('length', axis=1)
 
-                partial_pearsonr = partial(pearsonr, y=df_group_no_length[target_variable])
-                df_target_cors[group] = df_group_no_length.apply(lambda s:
-                                                                 partial_pearsonr(s)[0])
+                    partial_pearsonr = partial(pearsonr, y=df_group_no_length[target_variable])
+                    df_target_cors[group] = df_group_no_length.apply(lambda s:
+                                                                     partial_pearsonr(s)[0])
 
-                df_target_pcorr[group] = partial_correlations(df_group_no_length)[target_variable]
-                pcor_dict = {}
-                columns = [c for c in df_group.columns if c not in ['sc1', 'length']]
-                for c in columns:
-                    pcor_dict[c] = partial_correlations(df_group[[c, 'sc1', 'length']])['sc1'][c]
-                df_target_pcorr_no_length[group] = pd.Series(pcor_dict)
+                    df_target_pcorr[group] = partial_correlations(df_group_no_length)[target_variable]
+                    pcor_dict = {}
+                    columns = [c for c in df_group.columns if c not in ['sc1', 'length']]
+                    for c in columns:
+                        pcor_dict[c] = partial_correlations(df_group[[c, 'sc1', 'length']])['sc1'][c]
+                    df_target_pcorr_no_length[group] = pd.Series(pcor_dict)
 
         # remove the row containing the correlation of the target variable
         # with itself and take the transpose
@@ -685,7 +692,9 @@ class Analyzer:
         df = pd.DataFrame({'human': human_scores,
                            'system': system_scores}).dropna(how='any')
 
-        if len(df['human'].unique()) == 1 or len(df['system'].unique()) == 1:
+        if len(df) == 1:
+            correlations = np.nan
+        elif len(df['human'].unique()) == 1 or len(df['system'].unique()) == 1:
             correlations = np.nan
         else:
             correlations = pearsonr(df['human'], df['system'])[0]
@@ -724,8 +733,13 @@ class Analyzer:
                                                population_system_score_sd,
                                                method=smd_method)
 
-        # compute r2 and MSE
-        r2 = r2_score(human_scores, system_scores)
+        # compute r2
+        if len(df) == 1:
+            r2 = np.nan
+        else:
+            r2 = r2_score(human_scores, system_scores)
+        
+        # compute MSE
         mse = mean_squared_error(human_scores, system_scores)
         rmse = np.sqrt(mse)
 

tests/test_analyzer.py
@@ -31,6 +31,12 @@ class TestAnalyzer:
         self.df_features_same_score = self.df_features.copy()
         self.df_features_same_score[['sc1']] = [3] * 10
 
+        self.df_features_with_groups = self.df_features.copy()
+        self.df_features_with_groups['group'] = ['group1']*5 + ['group2']*5
+
+        self.df_features_with_groups_and_length = self.df_features_with_groups.copy()
+        self.df_features_with_groups_and_length['length'] = self.prng.normal(50, 250, 10)
+
         self.human_scores = pd.Series(self.prng.randint(1, 5, size=10))
         self.system_scores = pd.Series(self.prng.random_sample(10) * 5)
         self.same_human_scores = pd.Series([3] * 10)
@@ -47,12 +53,9 @@ class TestAnalyzer:
 
     def test_that_correlation_helper_works_for_data_with_one_row(self):
         # this should return two data frames with nans
-        # we expect a runtime warning here so let's suppress it
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=RuntimeWarning)
-            retval = Analyzer.correlation_helper(self.df_features[:1], 'sc1', 'group')
-            assert_equal(retval[0].isnull().values.sum(), 3)
-            assert_equal(retval[1].isnull().values.sum(), 3)
+        retval = Analyzer.correlation_helper(self.df_features[:1], 'sc1', 'group')
+        assert_equal(retval[0].isnull().values.sum(), 3)
+        assert_equal(retval[1].isnull().values.sum(), 3)
 
     def test_that_correlation_helper_works_for_data_with_two_rows(self):
         # this should return 1/-1 for marginal correlations and nans for
@@ -75,9 +78,41 @@ class TestAnalyzer:
         assert_equal(retval[0].isnull().values.sum(), 0)
         assert_almost_equal(np.abs(retval[1].values).sum(), 0.9244288637889855)
 
-    def test_that_correlation_helper_works_for_data_with_the_same_label(self):
 
-        # this should return two data frames with nans
+    def test_that_correlation_helper_works_for_data_with_groups(self):
+        retval = Analyzer.correlation_helper(self.df_features_with_groups, 'sc1', 'group')
+        assert_equal(len(retval[0]), 2)
+        assert_equal(len(retval[1]), 2)
+
+    
+    def test_that_correlation_helper_works_for_data_with_one_group_with_one_row(self):
+        # this should return a data frames with nans for group with 1 row
+        retval = Analyzer.correlation_helper(self.df_features_with_groups[:6], 'sc1', 'group')
+        print(retval)
+        assert_equal(len(retval[0]), 2)
+        assert_equal(len(retval[1]), 2)
+        assert_equal(retval[0].isnull().values.sum(), 3)
+
+    def test_that_correlation_helper_works_for_data_with_groups_and_length(self):
+        retval = Analyzer.correlation_helper(self.df_features_with_groups_and_length, 
+                                             'sc1', 'group', include_length=True)
+        for df in retval:
+            assert_equal(len(df), 2)
+            assert_equal(len(df.columns), 3)
+        
+    
+    def test_that_correlation_helper_works_for_data_with_one_group_with_one_row(self):
+        # this should return a data frames with nans for group with 1 row
+        retval = Analyzer.correlation_helper(self.df_features_with_groups_and_length[:6], 
+                                             'sc1', 'group', include_length=True)
+        for df in retval:
+            assert_equal(len(df), 2)
+            assert_equal(len(df.columns), 3)
+
+
+    
+
+    def test_that_correlation_helper_works_for_data_with_the_same_label(self):
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', category=RuntimeWarning)
             retval = Analyzer.correlation_helper(self.df_features_same_score, 'sc1', 'group')
@@ -92,7 +127,8 @@ class TestAnalyzer:
             warnings.filterwarnings('ignore', category=RuntimeWarning)
             evals = Analyzer.metrics_helper(self.human_scores[0:1],
                                             self.system_scores[0:1])
-            assert_equal(evals.isnull().values.sum(), 4)
+            print(evals)
+            assert_equal(evals.isnull().values.sum(), 5)
 
     def test_that_metrics_helper_works_for_data_with_the_same_label(self):
         # There should be NaNs for correlation and SMD.
@@ -149,7 +185,7 @@ class TestAnalyzer:
             df[i] = df['a'] * i
         (components, variance) = Analyzer.compute_pca(df, df.columns)
         assert_equal(len(components.columns), 50)
-        assert_equal(len(variance.columns), 50)
+        assert_equal(len(components.columns), 50)
 
     def test_compute_disattenuated_correlations_single_human(self):
         hm_corr = pd.Series([0.9, 0.8, 0.6],