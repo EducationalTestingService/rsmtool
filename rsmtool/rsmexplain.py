0ef94c7f7ff8e71423cc34c4136d516cd5620584
remo-help <61247737+remo-help@users.noreply.github.com>
2022-07-08 19:54:16 -0700

    Create a stub file for rsmexplain

---
+++ rsmtool/rsmexplain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import logging
+import shap
+from skll.learner import Learner
+
+
+def generate_explan():
+    """
+    Generates the explanation object.
+    :return:
+    """
+    return None
+
+
+def generate_report():
+    """
+    Generates a report and saves the SHAP_values and aexplanation object to disk
+    :return:
+    """
+
+
+def main():
+    return None
+
+
+if __name__ == '__main__':
+    main()
