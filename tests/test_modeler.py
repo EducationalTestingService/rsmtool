78ddd5fdf16913a38b488fe96f2001595ec79eb0
Nitin Madnani <nmadnani@ets.org>
2023-01-06 15:58:50 -0500

    Make `Modeler.predict()` more flexible.
    - Make `min_score` and `max_score` optional parameters
    - Check that the optional parameters are specified if `predict_expected`
      is `True`, since we do not need them otherwise.
    - Add a new unit test for this new check.

--- tests/test_modeler.py
+++ tests/test_modeler.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from nose.tools import eq_
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_raises_regex
+
 from rsmtool.modeler import Modeler
 
 
@@ -37,3 +38,10 @@ class TestModeler:
     def test_get_feature_names_is_none(self):
         modeler = Modeler()
         eq_(modeler.get_feature_names(), None)
+
+    def test_expected_scores_no_min_max(self):
+        df = pd.DataFrame([{"spkitemid": "DUMMY", "A": 1, "B": 2}])
+        with assert_raises_regex(
+            ValueError, r"Must specify 'min_score' and 'max_score' for expected scores."
+        ):
+            self.modeler.predict(df, predict_expected=True)
