0e295c0427b5651abb451492b001cf2ddfec8e11
Nitin Madnani <nmadnani@ets.org>
2023-05-21 14:32:58 -0400

    feat: add range parsing function for rsmexplain.

--- rsmtool/utils/conversion.py
+++ rsmtool/utils/conversion.py
@@ -10,6 +10,8 @@ Utility classes and functions for type conversion.
 
 from skll.data import safe_float as string_to_number
 
+from .constants import RSMEXPLAIN_RANGE_REGEXP
+
 
 def int_to_float(value):
     """
@@ -43,3 +45,40 @@ def convert_to_float(value):
         Value converted to float, if possible
     """
     return int_to_float(string_to_number(value))
+
+
+def parse_range(value):
+    """
+    Parse range strings used in rsmexplain.
+
+    Valid range strings are two positive integers separated by a hyphen ("10-20").
+
+    Parameters
+    ----------
+    value : str
+        The range string we want to parse.
+
+    Returns
+    -------
+    Tuple[int, int]
+        A tuple containing two integers.
+    """
+    valid_range = False
+    if matchobj := RSMEXPLAIN_RANGE_REGEXP.match(value):
+        groups = matchobj.groupdict()
+        start_index = string_to_number(groups.get("start"))
+        end_index = string_to_number(groups.get("end"))
+
+        # the range is valid if and only if:
+        # - both start and end indices are positive integers
+        # - and start index < end index
+        valid_range = (
+            isinstance(start_index, int)
+            and isinstance(end_index, int)
+            and 0 <= start_index < end_index
+        )
+
+    if not valid_range:
+        raise ValueError(f"invalid value '{value}' specified for range")
+    else:
+        return [start_index, end_index]
