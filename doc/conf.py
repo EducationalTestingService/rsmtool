f682db9f81de492db41dcd65c5650be664c1441b
Nitin Madnani <nmadnani@ets.org>
2023-07-24 10:38:39 -0400

    chore: use `sphinx_design` in docs.

--- doc/conf.py
+++ doc/conf.py
@@ -40,6 +40,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_design",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
