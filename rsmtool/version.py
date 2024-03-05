0577c68d04a16402a728f465c5965c6ca111506c
Nitin Madnani <nmadnani@ets.org>
2024-03-04 21:45:31 -0500

    chore: update version number

--- rsmtool/version.py
+++ rsmtool/version.py
@@ -5,5 +5,5 @@ This module exists solely for version information so we only have to change it
 in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
 """
 
-__version__ = "11.3.0"
+__version__ = "12.0.0"
 VERSION = tuple(int(x) for x in __version__.split("."))
